54a08384861f0219f480a5d72be5dcb9dc8a07f5
kodywang <553630288@qq.com>
2019-05-30 16:31:07 +0800

    init project

---
+++ src/pages/index/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="home-container">
+    <div class="title">小游戏集合</div>
+    <div class="menu-wrap">
+      <div href="index.html#/snake" class="menu-item">贪吃蛇</div>
+      <div href="index.html#/eliminate" class="menu-item">消消乐</div>
+      <div href="index.html#/pop-block" class="menu-item">消灭方块</div>
+      <div href="index.html#/drag-block" class="menu-item">拖动方块</div>
+      <div href="index.html#/color" class="menu-item">烧脑颜色</div>
+      <div href="index.html#/pictire" class="menu-item">拼图</div>
+      <div @click="gotoPage('/pages/number_eliminate/main')" class="menu-item">数字消消乐</div>
+      <div href="index.html#/draw" class="menu-item">画板</div>
+      <div href="index.html#/date" class="menu-item">日历</div>
+    </div>
+    <div class="abort-us">
+      <!-- <a href="/abort-us">关于作品</a> -->
+      <a href="index.html#/abort-us">关于作品</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    gotoPage (page) {
+      wx.navigateTo({
+        url: page
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home-container{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url('https://images.pexels.com/photos/18174/reflection-pad-gaming-gamepad.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  overflow: scroll;
+}
+
+.abort-us {
+  padding: 20px;
+  text-align: center;
+}
+
+.abort-us div {
+  text-decoration-line: none;
+  color: #333;
+  text-shadow: #7777d8 2px 2px 10px;
+}
+
+.title {
+  text-align: center;
+  font-size: 28px;
+  margin: 75px 0;
+  color: #fff;
+  text-shadow: #7777d8 2px 2px 10px;
+}
+
+.menu-wrap {
+  width: 60%;
+  margin: auto;
+}
+
+.menu-item {
+  display: block;
+  text-decoration-line: none;
+  border-radius: 25px;
+  background-color: #7777d8;
+  height: 40px;
+  line-height: 40px;
+  margin-bottom: 15px;
+  text-align: center;
+  color: #fff;
+  box-shadow: #7777d9 0 0 6px 0px;
+}
+</style>
---
+++ src/pages/number_eliminate/index.vue
@@ -0,0 +1,294 @@
+<template>
+  <div>
+    <div class="source-wrap">得分：{{source}}</div>
+    <div class="game-panel"
+      catchtouchmove
+      @touchstart="start"
+      @touchmove="move"
+      @touchend="end">
+      <div class="col-wrap" v-for="(item, index) in blockCount * blockCount" :key="index">
+        <div class="col" :class="'num-' + blockNumbers[index]" v-if="blockNumbers[index]" :data-index="index">{{blockNumbers[index]}}</div>
+      </div>
+    </div>
+
+    <div class="rule-wrap">
+      <div>游戏规则：</div>
+      <div>移动数字，三个及以上相邻的数字会消除</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import _ from 'underscore'
+
+export default {
+  data () {
+    return {
+      source: 0,
+      blockCount: 5,
+      startIndex: '',
+      initNumber: [1, 2, 3, 4, 5, 6],
+      blockNumbers: {}
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    start (e) {
+      console.log(e)
+      this.startX = e.mp.changedTouches[0].pageX
+      this.startY = e.mp.changedTouches[0].pageY
+      this.startIndex = parseInt(e.target.dataset.index)
+    },
+    move (e) {
+      e.preventDefault()
+    },
+    end (e) {
+      e.preventDefault()
+      this.endX = e.mp.changedTouches[0].pageX
+      this.endY = e.mp.changedTouches[0].pageY
+
+      let moveX = this.endX - this.startX
+      let moveY = this.endY - this.startY
+
+      if (Math.abs(moveX) > Math.abs(moveY)) {
+        if (moveX > 0) {
+          console.log('右')
+          this.moveBlock(this.startIndex + 1)
+        } else if (moveX < 0) {
+          console.log('左')
+          this.moveBlock(this.startIndex - 1)
+        }
+      } else if (Math.abs(moveX) < Math.abs(moveY)) {
+        if (moveY > 0) {
+          console.log('下')
+          this.moveBlock(this.startIndex + this.blockCount)
+        } else if (moveY < 0) {
+          console.log('上')
+          this.moveBlock(this.startIndex - this.blockCount)
+        }
+      }
+    },
+    moveBlock (targetIndex) {
+      if (this.cleaning) {
+        return
+      }
+      let xIndex = this.startIndex % this.blockCount
+      if (xIndex === 4 && targetIndex % this.blockCount === 0) {
+        return
+      } else if (xIndex === 0 && targetIndex % this.blockCount === 4) {
+        return
+      } else if (targetIndex > (this.blockCount * this.blockCount) || targetIndex < 1) {
+        return
+      }
+
+      var blockNumbers = Object.assign({}, this.blockNumbers)
+      let startNumber = blockNumbers[this.startIndex]
+      blockNumbers[this.startIndex] = blockNumbers[targetIndex]
+      blockNumbers[targetIndex] = startNumber
+
+      this.blockNumbers = blockNumbers
+
+      setTimeout(() => {
+        this.checkNearBlock()
+      }, 400)
+    },
+    checkNearBlock () {
+      let hasNeedClearBlock = false
+      for (let index = 0; index < this.blockCount * this.blockCount; index++) {
+        let nearBlock = [index]
+        let blockStark = [index]
+        do {
+          let surroundBlocks = this.getSurroundBlock(blockStark.pop(), nearBlock)
+          for (let blockIndex of surroundBlocks) {
+            if (_.indexOf(nearBlock, blockIndex) === -1) {
+              nearBlock.push(blockIndex)
+              blockStark.push(blockIndex)
+            }
+          }
+        } while (blockStark.length > 0)
+        if (nearBlock.length >= 3) {
+          hasNeedClearBlock = true
+          this.cleanBlock(nearBlock)
+          break
+        }
+      }
+      if (!hasNeedClearBlock) {
+        this.cleaning = false
+      }
+    },
+    getSurroundBlock (index, needCleanBlock) {
+      let number = this.blockNumbers[index]
+      let surroundBlock = []
+      if (index % this.blockCount !== 4 && _.indexOf(needCleanBlock, index + 1) === -1 && number === this.blockNumbers[index + 1]) {
+        surroundBlock.push(index + 1)
+      }
+      if (index % this.blockCount !== 0 && _.indexOf(needCleanBlock, index - 1) === -1 && number === this.blockNumbers[index - 1]) {
+        surroundBlock.push(index - 1)
+      }
+      if (_.indexOf(needCleanBlock, index + this.blockCount) === -1 && number === this.blockNumbers[index + this.blockCount]) {
+        surroundBlock.push(index + this.blockCount)
+      }
+      if (_.indexOf(needCleanBlock, index - this.blockCount) === -1 && number === this.blockNumbers[index - this.blockCount]) {
+        surroundBlock.push(index - this.blockCount)
+      }
+      return surroundBlock
+    },
+    cleanBlock (blocks) {
+      console.log(blocks)
+      this.cleaning = true
+      let currentIndex = blocks[0]
+      let number = this.blockNumbers[currentIndex] + 1
+      for (let index of blocks) {
+        this.$set(this.blockNumbers, index, '')
+      }
+      this.$set(this.blockNumbers, currentIndex, number)
+      this.source += (blocks.length * number)
+      this.fullBlock(blocks, currentIndex).then(
+        () => {
+          setTimeout(() => {
+            this.checkNearBlock()
+          }, 700)
+        }
+      )
+    },
+    fullBlock (blocks, currentIndex) {
+      var blockNumbers = Object.assign({}, this.blockNumbers)
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          for (let index of blocks) {
+            if (index !== currentIndex) {
+              blockNumbers[index] = _.sample(this.initNumber)
+            }
+          }
+          this.blockNumbers = blockNumbers
+          resolve()
+        }, 700)
+      })
+    },
+    init () {
+      for (let w = 0; w < this.blockCount * this.blockCount; w++) {
+        this.blockNumbers[w] = _.sample(this.initNumber)
+      }
+      this.checkNearBlock()
+      console.log(_.groupBy(this.blockNumbers))
+    }
+  }
+}
+</script>
+
+<style scoped>
+.source-wrap {
+  padding: 30px;
+  display: flex;
+  align-items: center;
+  color: #555;
+  font-weight: 600;
+  justify-content: center;
+}
+
+.rule-wrap {
+  padding: 15px;
+  color: #ccc;
+  font-size: 15px;
+}
+
+.game-panel {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: auto;
+  width: 80vw;
+  height: 80vw;
+  max-width: 375px;
+  max-height: 375px;
+  background-color: #444;
+  box-shadow: 0 2px 10px #bbb;
+}
+
+.game-panel .col-wrap {
+  background-color: #fff;
+  width: 18%;
+  height: 18%;
+}
+
+.game-panel .col {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+}
+
+.col:active {
+  transform: scale(0.9)
+}
+
+.game-panel .num-1 {
+  background-color: #FFE4B5;
+  color: #fff;
+  animation: trans-animation .5s;
+}
+
+.game-panel .num-2 {
+  background-color: #FFC0CB;
+  color: #fff;
+  animation: trans-animation .5s;
+}
+
+.game-panel .num-3 {
+  background-color: #FF1493;
+  color: #E0E0E0;
+  animation: trans-animation .5s;
+}
+
+.game-panel .num-4 {
+  background-color: #EEB422;
+  color: #FFF;
+  animation: trans-animation .4s;
+}
+
+.game-panel .num-5 {
+  background-color: #E0E0E0;
+  color: #fff;
+  animation: trans-animation .5s;
+}
+
+.game-panel .num-6 {
+  background-color: #D8BFD8;
+  color: #fff;
+  animation: trans-animation .4s;
+}
+
+.game-panel .num-7 {
+  background-color: #CDAD00;
+  color: #FFF;
+  animation: trans-animation .4s;
+}
+
+.game-panel .num-8 {
+  background-color: #CFF5CD;
+  color: #FFF;
+  animation: trans-animation .3s;
+}
+
+.game-panel .num-9 {
+  background-color: #CD5B45;
+  color: #FFF;
+  animation: trans-animation .3s;
+}
+
+.game-panel .num-10 {
+  background-color: #B23AEE;
+  color: #FFF;
+  animation: trans-animation .3s;
+}
+
+@keyframes trans-animation {
+  from {transform: translateY(-200%)}
+  to {transform: translateY(0)}
+}
+</style>
